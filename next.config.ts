219e4d4c8d84115a4bd872dd988cc279b97bb925
sangheonkimme <sangheon1646@gmail.com>
2025-09-19 19:43:31 +0900

    Fix : CORS 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://port-0-account-book-node-mfh2zrw921687152.sel3.cloudtype.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
